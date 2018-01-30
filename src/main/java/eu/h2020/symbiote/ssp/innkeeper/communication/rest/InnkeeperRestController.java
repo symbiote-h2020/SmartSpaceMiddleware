66b71380156f99909240b61b769ea745ac7248fc
Fabrizio Giuliano <fabrizio.giuliano@cnit.it>
2018-01-30 17:26:50 +0100

    changes in InnkeeperRestController

--- src/main/java/eu/h2020/symbiote/ssp/innkeeper/communication/rest/InnkeeperRestController.java
+++ src/main/java/eu/h2020/symbiote/ssp/innkeeper/communication/rest/InnkeeperRestController.java
@@ -3,36 +3,10 @@ package eu.h2020.symbiote.ssp.innkeeper.communication.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
 
-import eu.h2020.symbiote.security.ClientSecurityHandlerFactory;
-import eu.h2020.symbiote.security.ComponentSecurityHandlerFactory;
-import eu.h2020.symbiote.security.commons.Certificate;
-import eu.h2020.symbiote.security.commons.Token;
-import eu.h2020.symbiote.security.commons.credentials.AuthorizationCredentials;
-import eu.h2020.symbiote.security.commons.credentials.BoundCredentials;
-import eu.h2020.symbiote.security.helpers.CryptoHelper;
-import eu.h2020.symbiote.security.helpers.MutualAuthenticationHelper;
-import eu.h2020.symbiote.security.commons.exceptions.custom.AAMException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.InvalidArgumentsException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.JWTCreationException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.NotExistingUserException;
 import eu.h2020.symbiote.security.commons.exceptions.custom.SecurityHandlerException;
 import eu.h2020.symbiote.security.commons.exceptions.custom.ValidationException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.WrongCredentialsException;
 import eu.h2020.symbiote.security.communication.IAAMClient;
-import eu.h2020.symbiote.security.communication.payloads.AAM;
-import eu.h2020.symbiote.security.communication.payloads.AvailableAAMsCollection;
-import eu.h2020.symbiote.security.communication.payloads.CertificateRequest;
-import eu.h2020.symbiote.security.communication.payloads.SecurityRequest;
-import eu.h2020.symbiote.security.handler.IComponentSecurityHandler;
-import eu.h2020.symbiote.security.handler.ISecurityHandler;
-import eu.h2020.symbiote.security.communication.AAMClient;
-
-import eu.h2020.symbiote.ssp.communication.rabbit.SSPRecourceCreatedOrUpdated;
 import eu.h2020.symbiote.ssp.communication.rest.*;
 import eu.h2020.symbiote.ssp.exception.InvalidMacAddressException;
 import eu.h2020.symbiote.ssp.innkeeper.model.InnkeeperResource;
@@ -43,11 +17,11 @@ import eu.h2020.symbiote.ssp.innkeeper.repository.ResourceRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.bson.types.ObjectId;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -61,17 +35,10 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.cert.CertificateException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Timer;
 import java.util.stream.Collectors;
 
@@ -107,37 +74,7 @@ public class InnkeeperRestController {
 
 	@Value("${rabbit.routingKey.rap.sspResourceDeleted}")
 	private String rapSSPResourceDeletedRoutingKey;
-	
-    
 
-    @Value("${rap.mongo.host}")
-    private String db_host;
-    @Value("rap.mongo.port")
-	private String db_port;
-    @Value("${rap.mongo.user}")
-	private String db_user;
-	@Value("rap.mongo.pass")
-	private String db_pass;
-	@Value("${rap.mongo.dbname}")
-    private String db_name;
-	
-	private MongoClient db_client;
-	    
-
-	private MongoDatabase dbconnect() {
-		log.info("-----------------------------");
-		log.info("db_user	=	"+db_user);
-		log.info("db_pass	=	"+db_pass);
-		log.info("db_host	=	"+db_host);
-		log.info("db_port	=	"+db_port);
-		log.info("db_name	=	"+db_name);
-		log.info("-----------------------------");
-		MongoClientURI uri  = new MongoClientURI("mongodb://"+db_user+":"+db_pass+"+@"+db_host+":"+db_port+"/"+db_name); 
-		log.info("URI:"+uri)
-		db_client = new MongoClient(uri);
-        MongoDatabase db = db_client.getDatabase(uri.getDatabase());
-        return db;
-	}
 
 	@Autowired
 	public InnkeeperRestController(ResourceRepository resourceRepository, RabbitTemplate rabbitTemplate,
@@ -168,13 +105,22 @@ public class InnkeeperRestController {
 		Assert.notNull(offlineTimerTaskMap, "offlineTimerTaskMap can not be null!");
 		this.offlineTimerTaskMap = offlineTimerTaskMap;
 		
-		dbconnect();
+		//dbconnect();
 	}
 
-	// @PostMapping(InnkeeperRestControllerConstants.INNKEEPER_JOIN_REQUEST_PATH)
+	
+	public ResponseEntity<Object> setResponseJoinEntity() {
+		HttpHeaders headers =new HttpHeaders();
+		headers.add("1", "JOINuno");
+		headers.add("2", "due");
+		headers.add("7", "settebbabbau");
+		String mybody="<html>THIS IS A BODY</html>\n";
+		ResponseEntity<Object> myresponse = new ResponseEntity<>(mybody,headers, HttpStatus.OK);
+		return myresponse;
+	}	
 	@RequestMapping(value = InnkeeperRestControllerConstants.INNKEEPER_JOIN_REQUEST_PATH, method = RequestMethod.POST)
-
-	public ResponseEntity<Object> join(@RequestParam Map<String, String> requestParams) {
+	public ResponseEntity<Object> join(@RequestBody Map<String, String> requestParams) throws NoSuchAlgorithmException, SecurityHandlerException, ValidationException, JsonProcessingException {
+//		public ResponseEntity<Object> join(@RequestParam Map<String, String> requestParams) {
 		log.info("New join request was received  requestParams = " + requestParams);
 		String id = requestParams.get("id");
 		String mac = requestParams.get("mac");
@@ -188,12 +134,25 @@ public class InnkeeperRestController {
 		log.info("------------------------------------");
 		
 		
-		return null;
+		return this.setResponseJoinEntity();
 
+	}
+	
+		
+	public ResponseEntity<Object> setResponseRegistryEntity() {
+		HttpHeaders headers =new HttpHeaders();
+		headers.add("1", "uno");
+		headers.add("2", "due");
+		headers.add("7", "settebbabbau");
+		String mybody="<html>THIS IS A BODY</html>\n";
+		ResponseEntity<Object> myresponse = new ResponseEntity<>(mybody,headers, HttpStatus.OK);
+		return myresponse;
 	}
 
 	@RequestMapping(value = InnkeeperRestControllerConstants.INNKEEPER_REGISTRY_REQUEST_PATH, method = RequestMethod.POST)
-	public ResponseEntity<Object> registry(@RequestParam Map<String, String> payload) throws NoSuchAlgorithmException, SecurityHandlerException, ValidationException, JsonProcessingException {
+//	public ResponseEntity<Object> registry(@RequestParam Map<String, String> payload) throws NoSuchAlgorithmException, SecurityHandlerException, ValidationException, JsonProcessingException {
+	public ResponseEntity<Object> registry(@RequestBody Map<String, String> payload) throws NoSuchAlgorithmException, SecurityHandlerException, ValidationException, JsonProcessingException {
+
 		// Object response=requestParams;
 		log.info("New registry request was received  payload = " + payload);
 		String id = payload.get("id");
@@ -203,97 +162,25 @@ public class InnkeeperRestController {
 		String informationModel = payload.get("informationModel");
 
 		log.info("------------------------------------");
-		log.info("id          = " + id);
-		log.info("name        = " + name);
-		log.info("description = " + description);
-		log.info("url         = " + url);
+		log.info("id          		= " + id);
+		log.info("name        		= " + name);
+		log.info("description 		= " + description);
+		log.info("url         		= " + url);
+		log.info("informationModel	= " + informationModel);
 		log.info("------------------------------------");
-
-		
-		/*
-		IComponentSecurityHandler rhCSH = ComponentSecurityHandlerFactory.getComponentSecurityHandler(
-				coreAAMAddress,
-				KEY_STORE_PATH,
-				KEY_STORE_PASSWORD,
-				"reghandler" + "@platfom1",
-				localAAMAddress,
-				false,
-				componentOwnerUsername,
-				componentOwnerPassword);
-
-		// building a service response that needs to be attached to each of your business responses
-		String regHandlerServiceResponse = rhCSH.generateServiceResponse();
-
-		// building a security request to authorize operation from the registration handler in other platforms' components (e.g. Core Registry)
-		SecurityRequest rhSecurityRequest = rhCSH.generateSecurityRequestUsingLocalCredentials();
-		
-		
-		*/
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-		// creating REST client communicating with SymbIoTe Authorization Services 
-		// AAMServerAddress can be acquired from SymbIoTe web page
-//		String coreAAMAddress="https://symbiote.man.poznan.pl:8100";
-		String coreAAMAddress="https://symbiote.man.poznan.pl:8100/coreInterface/v1";
-		
-
-		/*		
-		aamClient = new AAMClient(coreAAMAddress);
-
-
-	
-		// acquiring Guest Token
-		String guestToken="";
-		SecurityRequest securityRequest = null;
-		Map<String, String> securityHeaders = new HashMap<>();
-
-		try {
-			// creating securityRequest using guest Token
-			securityRequest = new SecurityRequest(guestToken);
-			securityHeaders = securityRequest.getSecurityRequestHeaderParams();
-
-			guestToken = aamClient.getGuestToken();
-
-		} catch (JWTCreationException | AAMException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("HEADER:");
-		System.out.println(securityHeaders.toString());
-		 */	
-	
-	
-		
-		
-		
-		
-		//System.out.println(token.getToken());
-		//System.out.println(token.getId());		
-		
-		//Certificate clientCertificate = clientSH.getCertificate(coreAAM, "fgiuliano", "qwerty123456", "fabrizio-cnit");
 
 		// TODO: when a registry request arrives innkeeper forward those info to the
 		// Core
+		// 1. forge a response to symbiote agent
+		
 		// 1. LAAM Request
 
 		// 2. Core Registration (forward payload)
 
 		// 3. SSP-RAP Registration (forward payload)
 
-		return null;
+
+		return this.setResponseRegistryEntity();
 	}
 
 	/*
